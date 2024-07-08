b2b3b43447546513747235235a2787a35f929f97
c4710n <c4710n@users.noreply.github.com>
2024-07-08 22:31:34 +0800

    init: mix new jxon --module JXON

---
+++ lib/jxon.ex
@@ -0,0 +1,18 @@
+defmodule JXON do
+  @moduledoc """
+  Documentation for `JXON`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JXON.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jxon_test.exs
@@ -0,0 +1,8 @@
+defmodule JXONTest do
+  use ExUnit.Case
+  doctest JXON
+
+  test "greets the world" do
+    assert JXON.hello() == :world
+  end
+end
