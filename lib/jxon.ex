6a420c30f805c7e938dca11cee063177e86c14f5
c4710n <c4710n@users.noreply.github.com>
2024-07-08 22:59:43 +0800

    feat: add support for encoding / decoding

--- lib/jxon.ex
+++ lib/jxon.ex
@@ -1,18 +1,60 @@
 defmodule JXON do
   @moduledoc """
-  Documentation for `JXON`.
+  A wrapper for various JSON implementations.
   """
 
-  @doc """
-  Hello world.
+  @spec encode(term()) :: {:ok, String.t()} | :error
+  def encode(term) do
+    __encode__(term)
+  end
 
-  ## Examples
+  @spec decode(binary()) :: {:ok, term()} | :error
+  def decode(binary) when is_binary(binary) do
+    __decode__(binary)
+  end
 
-      iex> JXON.hello()
-      :world
+  cond do
+    Code.ensure_loaded?(Jason) ->
+      defp __encode__(term) do
+        case Jason.encode(term) do
+          {:ok, string} -> {:ok, string}
+          _ -> :error
+        end
+      end
 
-  """
-  def hello do
-    :world
+      defp __decode__(binary) do
+        case Jason.decode(term) do
+          {:ok, string} -> {:ok, string}
+          _ -> :error
+        end
+      end
+
+    Code.ensure_loaded?(:json) ->
+      defp __encode__(term) do
+        try do
+          {:ok, :json.encode(term) |> to_string()}
+        catch
+          _, _ ->
+            :error
+        end
+      end
+
+      defp __decode__(binary) do
+        try do
+          {:ok, :json.decode(binary)}
+        catch
+          _, _ ->
+            :error
+        end
+      end
+
+    true ->
+      defp __encode__(_term) do
+        raise "no JSON parser found"
+      end
+
+      defp __decode__(_binary) do
+        raise "no JSON parser found"
+      end
   end
 end
